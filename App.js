464b3c5ee338c2c361470dd444aa1f171b39e912
ambit741235 <76868481+ambit741235@users.noreply.github.com>
2021-12-11 17:59:42 +0530

    Adding Comments to App.js, Modifying app.json for App Icon and Package Name

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+//Importing React Module and other React Native with React Navigation Module
 import * as React from 'react';
+import { Animated } from 'react-native';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import AnimatedSplash from "react-native-animated-splash-screen";
 
+//Importing Screens from Local
 import LoginScreen from "./screens/LoginScreen";
 import LoadingScreen from "./screens/LoadingScreen";
 import DashboardScreen from "./screens/DashboardScreen";
 
+//Importing Complete Firebase
 import firebase from 'firebase';
 import { firebaseConfig } from "./config";
 
@@ -37,7 +41,7 @@ export default class App extends React.Component {
 
   render(){
     return (
-      <AnimatedSplash translucent={true} isLoaded={this.state.isLoaded} logoImage={require("./assets/logo.png")} backgroundColor={"#262626"} logoHeight={150} logoWidth={150}>
+      <AnimatedSplash translucent={true} isLoaded={this.state.isLoaded} logoImage={require("./assets/adaptive-icon.png")} backgroundColor={"#262626"}>
         <AppNavigator/>
       </AnimatedSplash>
     );
